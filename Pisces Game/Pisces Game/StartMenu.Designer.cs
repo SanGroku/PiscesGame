4f9481154503a181d2dd0eac93be4977904eb720
JackStrider <rossi.william@gmail.com>
2018-03-19 20:06:40 -0500

    Design work updated

--- Pisces Game/Pisces Game/StartMenu.Designer.cs
+++ Pisces Game/Pisces Game/StartMenu.Designer.cs
@@ -29,30 +29,17 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartMenu));
-            this.TitleLbl = new System.Windows.Forms.Label();
             this.StartBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // TitleLbl
-            // 
-            this.TitleLbl.AutoSize = true;
-            this.TitleLbl.BackColor = System.Drawing.Color.Transparent;
-            this.TitleLbl.Font = new System.Drawing.Font("Rockwell Extra Bold", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TitleLbl.ForeColor = System.Drawing.Color.LightSeaGreen;
-            this.TitleLbl.Location = new System.Drawing.Point(105, 116);
-            this.TitleLbl.Name = "TitleLbl";
-            this.TitleLbl.Size = new System.Drawing.Size(776, 113);
-            this.TitleLbl.TabIndex = 0;
-            this.TitleLbl.Text = "Pisces Game";
-            this.TitleLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // StartBtn
             // 
             this.StartBtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.StartBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StartBtn.Location = new System.Drawing.Point(412, 612);
+            this.StartBtn.Location = new System.Drawing.Point(549, 577);
+            this.StartBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StartBtn.Name = "StartBtn";
-            this.StartBtn.Size = new System.Drawing.Size(151, 60);
+            this.StartBtn.Size = new System.Drawing.Size(201, 74);
             this.StartBtn.TabIndex = 1;
             this.StartBtn.Text = "Start";
             this.StartBtn.UseVisualStyleBackColor = true;
@@ -60,26 +47,23 @@
             // 
             // StartMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSeaGreen;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.ClientSize = new System.Drawing.Size(1312, 937);
             this.Controls.Add(this.StartBtn);
-            this.Controls.Add(this.TitleLbl);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "StartMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pisces Game";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label TitleLbl;
         private System.Windows.Forms.Button StartBtn;
     }
 }
